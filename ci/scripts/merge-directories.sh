89322f3376287eaa438450dae33bd28fba037e81
Tim Green <timsgreen@gmail.com>
2019-08-29 12:04:53 -0400

    fix bug that used the wrong branch for version file

--- ci/scripts/merge-directories.sh
+++ ci/scripts/merge-directories.sh
@@ -3,5 +3,5 @@ set -e
 
 cp -p git-repo/Dockerfile image-build-dir/
 cp -pr git-repo/bin image-build-dir/
-cp -p git-repo/version image-build-dir/bin/
+cp -p version/version image-build-dir/bin/
 cp -p release-candidate/*.jar image-build-dir/
