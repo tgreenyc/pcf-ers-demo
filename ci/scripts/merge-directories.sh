d088f5d6b2eb606c1706783b0c3c970e381c3f76
Tim Green <timsgreen@gmail.com>
2019-08-29 21:27:29 -0400

    add api gateway support to cf pipeline

--- ci/scripts/merge-directories.sh
+++ ci/scripts/merge-directories.sh
@@ -2,6 +2,7 @@
 set -e
 
 cp -p git-repo/Dockerfile image-build-dir/
+cp -p git-repo/k8s-deploy.yml image-build-dir/
 cp -pr git-repo/bin image-build-dir/
 cp -p version/version image-build-dir/bin/
 cp -p release-candidate/*.jar image-build-dir/
